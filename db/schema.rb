482466d789e66987e9f4fa42987d1fd97876e34f
Rob Ingram <rob@headcrash.org>
2018-04-14 10:06:09 +1200

    Add devise to Author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405220039) do
+ActiveRecord::Schema.define(version: 20180413220115) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -23,10 +23,21 @@ ActiveRecord::Schema.define(version: 20180405220039) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_authors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
   end
 
   create_table "comments", force: :cascade do |t|
